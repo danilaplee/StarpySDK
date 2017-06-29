c8932d06092438d6d6b29a3113f7221b550deaaa
root <root@gce-chat.c.docdoc-01.internal>
2017-06-29 19:20:35 +0000

    fixes to gensdk

--- gensdk.sh
+++ gensdk.sh
@@ -4,7 +4,7 @@
 # Note use of -parameters option when compiling to record names
 #
 
-#cd `dirname $0`/src &&
+cd `dirname $0`/src &&
 
 #\rm `find src/me/starpy/sdk -name '*.class'` 2>/dev/null
 
@@ -12,20 +12,22 @@
 
 #\rm `find sdk-android/src/main/swift/Sources -name 'StarpySDK*.swift'` 2>/dev/null
 
-$JAVA_HOME/bin/javac -parameters ../src/me/starpy/sdk/*.java &&
+$JAVA_HOME/bin/javac -parameters me/starpy/sdk/*.java &&
 
-$JAVA_HOME/bin/jar cf /tmp/sdk.jar ../src/me/starpy/sdk/*.class &&
+$JAVA_HOME/bin/jar cf /tmp/sdk.jar me/starpy/sdk/*.class &&
+
+cd - &&
 
 # The last argument is the frameworks the project
 # is dependent on. java_lang for example will add
 # about 2mb to the app's apk and needs to be added
 # to the project's src/main/swift/Package.swift
 
-../genswift.sh me.starpy.sdk /tmp/sdk.jar "" &&
+./genswift.sh me.starpy.sdk /tmp/sdk.jar "" &&
 
 mkdir -p src/main/java/{me/starpy/sdk,org/genie/me_starpy_sdk} &&
 
-\cp -v ../src/me/starpy/sdk*.java src/main/java/me/starpy/sdk &&
-\cp -v ../src/org/genie/me_starpy_sdk/*.java src/main/java/org/genie/me_starpy_sdk &&
+\cp -v ./src/me/starpy/sdk*.java src/main/java/me/starpy/sdk &&
+\cp -v ./src/org/genie/me_starpy_sdk/*.java src/main/java/org/genie/me_starpy_sdk &&
 
-\cp -v ../me_starpy_sdk/Sources/*.swift ./sdk-android/src/main/swift/Sources
+\cp -v ./me_starpy_sdk/Sources/*.swift ./sdk-android/src/main/swift/Sources
